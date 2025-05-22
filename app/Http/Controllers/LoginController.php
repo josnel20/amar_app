755edd2eb94461f957093f312787a0943161d3f5
josnel20 <cosneljosnel91@gmail.com>
2025-05-22 13:28:32 -0300

    autenticacao do laraver pre-finnalizada

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
@@ -12,29 +13,23 @@ class LoginController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
-            'password' => 'required|string|min:6'
+            'password' => 'required|string|min:6',
         ]);
 
         if ($validator->fails()) {
             return response()->json(['level' => false, 'message' => 'Dados de login inválidos.', 'errors' => $validator->errors()], 422);
         }
-        
-        try {
-            if (User::where(['email' => $request->email, 'password' => bcrypt($request->password)])) {
-                $user = User::first();
 
-                //retornar para tela vue, lista de produtos
-                return response()->json(['level' => true, 'message' => 'Login realizado com sucesso!', 'data' => $user], 200);
-            } else {
-                return response()->json(['level' => false, 'message' => 'Credenciais inválidas.'], 401);
+        try {
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([ 'level' => false, 'message' => 'Credenciais inválidas.'], 401);
             }
 
+            $user = Auth::user();
+
+            return response()->json(['level' => true,'message' => 'Login realizado com sucesso!','data' => $user,'token' => $user->createToken('auth_token')->plainTextToken], 200);
         } catch (\Throwable $th) {
-            return response()->json([
-                'level' => false,
-                'message' => 'Ocorreu um erro ao tentar fazer login',
-                'error' => $th->getMessage()
-            ], 500);
+            return response()->json(['level' => false,'message' => 'Ocorreu um erro ao tentar fazer login.','error' => $th->getMessage()], 500);
         }
     }
 }
